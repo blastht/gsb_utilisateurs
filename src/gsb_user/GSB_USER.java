9b235d56f1e5bfe6c358f601340cf54b807af898
blastht <keitinicossavella@gmail.com>
2024-10-02 15:47:11 +0200

    initial commit

---
+++ src/gsb_user/GSB_USER.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gsb_user;
+
+/**
+ *
+ * @author Blast9870
+ */
+public class GSB_USER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
